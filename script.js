1d62f128fc1cdfda11f6d8b21a25eb45967eb353
Wesley Abdoul <Abdoulwes@gmail.com>
2024-01-05 13:16:40 +0100

    my

--- script.js
+++ script.js
@@ -4,6 +4,12 @@
 console.log("ADDITION-MASTER ™️");
 
 // 🦁 Utilise la fonction "prompt" pour demander le premier nombre à l'utilisateur
+const firstNumber = Number(await prompt (" Enter your name : "))
+
+const secondNumber = Number(await prompt (" Enter your name : "))
+
+console.log("Le résutat de l'addition est : ", firstNumber + secondNumber);
+
 // 🦁 Stocke le résultat dans une variable "firstNumber" et utilise `Number()` pour convertir la valeur en nombre
 // 💡 await prompt('Enter your name: ') permet de récupérer la valeur saisie par l'utilisateur
 
